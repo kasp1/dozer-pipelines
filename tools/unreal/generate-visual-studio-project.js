3b0c7de389958210d4f2d88c5a2111aa2863baac
Mirek Kaspar <k@e0.cz>
2022-03-18 12:57:15 +0000

    fix: updated generate-visual-studio-project.js to comply with Unreal Engine 5

--- tools/unreal/generate-visual-studio-project.js
+++ tools/unreal/generate-visual-studio-project.js
@@ -5,33 +5,56 @@
 //
 
 import { execSync } from 'child_process'
+import fs from 'fs'
 
-const UPROJECT = process.env['CI_UE_UPROJECT']
+const UPROJECT = process.argv[3] || process.env['CI_UE_UPROJECT']
+const UE_HOME = process.argv[2] || process.env['CI_UE_HOME']
+
+if (!(UE_HOME && UPROJECT)) {
+  console.log('Usage: node CI/shared/unreal/generate-sln.js <UE_HOME> <.uproject>')
+  process.exit(1)
+}
+
 const SLN = UPROJECT.replace('.uproject', '.sln')
 console.log(`##CI_UE_SLN=${SLN}#`)
 
-let cmd
-let projectRoot = process.cwd()
+let cmd, ubtPath
+const projectRoot = process.cwd()
+let uprojectPath = `${projectRoot}/${UPROJECT}`
+
+// UE4, win
+ubtPath = `${UE_HOME}/Engine/Binaries/DotNET/UnrealBuildTool.exe`
+
+if (!fs.existsSync(ubtPath)) {
+  // UE5, win
+  ubtPath = `${UE_HOME}/Engine/Binaries/DotNET/UnrealBuildTool/UnrealBuildTool.exe`
+  
+  // UE4, unix
+  if (!fs.existsSync(ubtPath)) {
+    // UE5, unix
+    ubtPath = `${UE_HOME}/Engine/Binaries/DotNET/UnrealBuildTool/UnrealBuildTool`
+    
+    if (!fs.existsSync(ubtPath)) {
+      console.log('UnrealBuildTool not found. Unreal Engine home:', UE_HOME)
+      process.exit(1)
+    }
+  }
+}
 
 if (process.platform == 'win32') {
-  cmd = [
-    '..\\UE4\\Engine\\Binaries\\DotNET\\UnrealBuildTool.exe',
-    '-projectfiles',
-    `-project=${projectRoot}/${UPROJECT}`,
-    '-game',
-    '-rocket',
-    '-progress'
-  ].join(' ')
-} else {
-  cmd = [
-    '../UE4/Engine/Binaries/DotNET/UnrealBuildTool.exe',
-    '-projectfiles',
-    `-project=${projectRoot}/${UPROJECT}`,
-    '-game',
-    '-rocket',
-    '-progress'
-  ].join(' ')
+  console.log('platform: ', process.platform == 'win32')
+  ubtPath = ubtPath.replace(/\//g, '\\')
+  uprojectPath = uprojectPath.replace(/\//g, '\\')
 }
+
+cmd = [
+  ubtPath,
+  '-projectfiles',
+  `-project=${uprojectPath}`,
+  '-game',
+  '-rocket',
+  '-progress'
+].join(' ')
 
 console.log(cmd)
 
